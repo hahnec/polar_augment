fd7de812818ebc4e85612a7de01c346698b77e0c
hahnec <christopher.hahne@unibe.ch>
2024-08-26 23:31:31 +0200

    fix(export): handle export

--- simulate_rotation_script.py
+++ simulate_rotation_script.py
@@ -12,9 +12,11 @@ from augmentations.rotation_raw import RandomPolarRotation
 if __name__ == '__main__':
 
     base_dir = Path('/media/chris/EB62-383C/CC_Rotation/')
-    plot_opt = True
+    plot_opt = False
     save_opt = True
-    skip_opt = False
+    skip_opt = True
+
+    np.random.seed(3008)
 
     # load measured rotation angles and center points
     with open(base_dir / 'angles_and_center_points.txt', 'r') as f:
@@ -43,6 +45,7 @@ if __name__ == '__main__':
 
     # iterate through angle measurements
     ys = []
+    errs = []
     for i, dir in enumerate(dir_list):
         intensity = read_cod_data_X3D(Path(dir) / 'raw_data' / '550nm' / '550_Intensite.cod', raw_flag=True)
         bruit = read_cod_data_X3D(Path(dir) / 'raw_data' / '550nm' /'550_Bruit.cod', raw_flag=True)
@@ -50,10 +53,10 @@ if __name__ == '__main__':
         F = torch.cat([B, A.moveaxis(-1, 0), W.moveaxis(-1, 0)], dim=0)
 
         random_idx = np.random.randint(0, i) if i > 0 else 0
+        print(random_idx)
         
         t = sum([transforms[i-idx][0] for idx in range(random_idx)]) if random_idx > 0 else transforms[i][0]
-        #angle = angle + float(t[0]) if i > 0 else 0
-        angle = float(t) if i > 0 else 0
+        angle = float(t) if random_idx > 0 else 0
         center = transforms.mean(0)[1:].tolist() #t[1:].tolist()
         result = rotate(F, angle=angle, center=center)
         f, m = result if not skip_opt else (result, rotate(pseudo_label, angle=angle, center=center))
@@ -62,32 +65,39 @@ if __name__ == '__main__':
             f[16:32][:, f[16:32].sum(0) == 0] = torch.eye(4, dtype=f.dtype).flatten()[:, None, None].repeat(1, f.shape[1], f.shape[2])[:, f[16:32].sum(0) == 0]
             f[32:][:, f[32:].sum(0) == 0] = torch.eye(4, dtype=f.dtype).flatten()[:, None, None].repeat(1, f.shape[1], f.shape[2])[:, f[32:].sum(0) == 0]
         y = mm_model(f[None])
-        y = cmap((y/y.max()).squeeze().numpy())
+        rgb = cmap((y/y.max()).squeeze().numpy())
 
         # unrotated
-        y_orig = mm_model(F[None])
-        y_orig = cmap((y_orig/y_orig.max()).squeeze().numpy())
-        imageio.imwrite('fig-'+str(i+1).zfill(2)+'_orig.png', norm_uint8(y_orig))
+        y_orig = mm_model(F[None]).squeeze().numpy()
+        #y_orig = cmap((y_orig/y_orig.max()))
         ys.append(y_orig)
 
-        if i > 0:
+        if i > 0 and random_idx > 0:
+            diff = (y.squeeze().numpy() - ys[-random_idx-1])/180*np.pi
+            errs.extend(diff[m.bool().squeeze().numpy()])
             if plot_opt:
                 fig, axs = plt.subplots(1, 3, figsize=(15, 8))
-                axs[0].imshow(y_orig.squeeze())
+                axs[0].imshow(cmap(y_orig/y_orig.max()).squeeze())
                 axs[0].set_title('Input')
-                axs[1].imshow(y[..., :3], alpha=1-m.squeeze())
+                axs[1].imshow(rgb[..., :3], alpha=m.squeeze().numpy())
                 axs[1].set_title('Rotated')
-                axs[2].imshow(ys[-random_idx-1].squeeze())
+                axs[2].imshow(cmap(ys[-random_idx-1]/ys[-random_idx-1].max()))
                 axs[2].set_title('GT')
                 plt.show()
             if save_opt:
-                rgb = norm_uint8(y)
+                rgb = norm_uint8(rgb)
                 alpha = norm_uint8(m.squeeze().numpy())
                 img = np.concatenate((rgb[..., :3], alpha[..., None]), axis=-1)
                 ext = ['_rect', '_wo'][skip_opt]
                 imageio.imwrite('fig-'+str(i).zfill(2)+ext+'.png', img)
+                imageio.imwrite('fig-'+str(i).zfill(2)+'_orig.png', norm_uint8(cmap(y_orig/y_orig.max())))
+                imageio.imwrite('fig-'+str(i).zfill(2)+'_gt.png', norm_uint8(cmap(ys[-random_idx-1]/ys[-random_idx-1].max())))
+                imageio.imwrite('fig-'+str(i).zfill(2)+'_diff.png', norm_uint8(np.stack((diff, diff, diff, alpha), axis=-1)))
         else:
             y_ref = y
 
         m_previous = m
         y_previous = y
+
+    print(np.round(np.mean(np.array(errs)**2)**.5, 4))
+    print(np.round(np.mean(abs(np.array(errs))), 4))
