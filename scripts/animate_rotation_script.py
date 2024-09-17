355891a52cee6fd3174ea273233c245d38f4ae0c
hahnec <inbox@christopherhahne.de>
2024-09-17 12:45:12 +0200

    refactor(path): change path

--- scripts/animate_rotation_script.py
+++ scripts/animate_rotation_script.py
@@ -11,7 +11,7 @@ from rotation_raw import RandomPolarRotation
 
 if __name__ == '__main__':
 
-    base_dir = Path('/media/chris/EB62-383C/CC_Rotation/')
+    base_dir = Path('/home/chris/Datasets/03_HORAO/CC_Rotation/')
     plot_opt = False
     save_opt = True
     skip_opt = False
